426508a70ed2161458daed1b76a4391fbc847cad
seankcheema <seankcheema@gmail.com>
2025-01-19 11:34:21 -0500

    cleaned up imports

--- personal-portfolio/src/components/Header.tsx
+++ personal-portfolio/src/components/Header.tsx
@@ -1,6 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
 import Hamburger from 'hamburger-react';
 import './Header.css';
 

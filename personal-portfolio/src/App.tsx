b188aa0980a527bc895229cf644c6e28d9ad8046
seancheema <seankcheema@gmail.com>
2025-04-10 20:22:41 -0400

    DM

--- personal-portfolio/src/App.tsx
+++ personal-portfolio/src/App.tsx
@@ -5,6 +5,7 @@ import Requestify from './components/Requestify.tsx';
 import TAAS from './components/TAAS.tsx';
 import BackToTop from './components/BackToTop.tsx'
 import FrontRunner from './components/FrontRunner.tsx';
+import DanceMarathon from './components/DanceMarathon.tsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/requestify" element={<Requestify />} />
           <Route path="/taas" element={<TAAS />} />
           <Route path="/frontrunner" element={<FrontRunner />} />
+          <Route path="/dance-marathon" element={<DanceMarathon />} />
         </Routes>
         <BackToTop/>
       </div>
